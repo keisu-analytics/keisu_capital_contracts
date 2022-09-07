8f08eff134ca5ce12c7fb2e3788361d74eeb66f4
DyedM1 <henry@hyf.one>
2022-09-07 16:55:55 -0400

    :wheelchair:

--- src/proxy/TransparentStaticProxy.sol
+++ src/proxy/TransparentStaticProxy.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 import "openzeppelin-contracts/contracts/proxy/Proxy.sol";
+import "openzeppelin-contracts/contracts/utils/StorageSlot.sol";
 
 contract TransparentStaticProxy is Proxy {
-    address public impl;
+    bytes32 internal constant implSlot =
+        0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     constructor(address _impl) payable {
-        impl = _impl;
+        StorageSlot.getAddressSlot(implSlot).value = _impl;
     }
 
     function _implementation() internal view override returns (address) {
-        return impl;
+        StorageSlot.getAddressSlot(implSlot).value;
     }
 }
