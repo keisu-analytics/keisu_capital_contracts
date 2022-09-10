6ee1968fdfd2efa1dda9c16dce77238e58233464
DyedM1 <henry@hyf.one>
2022-09-09 23:08:37 -0400

    formatting changes

--- src/proxy/TransparentStaticProxy.sol
+++ src/proxy/TransparentStaticProxy.sol
@@ -4,14 +4,15 @@ import "openzeppelin-contracts/contracts/proxy/Proxy.sol";
 import "openzeppelin-contracts/contracts/utils/StorageSlot.sol";
 
 contract TransparentStaticProxy is Proxy {
-    bytes32 internal constant implSlot =
+    //avoid collisions with implementation storage
+    bytes32 internal constant IMPLSLOT =
         0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     constructor(address _impl) payable {
-        StorageSlot.getAddressSlot(implSlot).value = _impl;
+        StorageSlot.getAddressSlot(IMPLSLOT).value = _impl;
     }
 
     function _implementation() internal view override returns (address) {
-        StorageSlot.getAddressSlot(implSlot).value;
+        return StorageSlot.getAddressSlot(IMPLSLOT).value;
     }
 }
