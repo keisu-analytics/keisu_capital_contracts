212e03fd74a2270eca079e948c768ecde43ffe90
DyedM1 <henry@hyf.one>
2022-09-11 18:26:04 -0400

    OrgValidatorCore complete

--- src/core/OrgValidatorCore.sol
+++ src/core/OrgValidatorCore.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
-import "openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
 
-contract OrgValidatorCore is EIP712 {
+contract OrgValidatorCore {
     struct Signature {
-        uint256 v;
-        uint256 r;
-        uint256 s;
+        uint256 actingRole;
+        address signer;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
     }
     struct Permission {
         uint128 role;
@@ -23,23 +24,32 @@ contract OrgValidatorCore is EIP712 {
         address member;
         uint256 role;
     }
+
+    struct ConfirmationCount {
+        uint128 role;
+        uint128 count;
+    }
     string public orgName;
+    bytes32 public domainSeperator;
     //Role id 0 refers to the Permissions template being used
     //Role id 1 refers to the Policies template being used
     mapping(uint256 => mapping(address => bool)) public roleMemberships;
     mapping(uint256 => uint256) public orgPermissions;
+    Policy[] public orgPolicies;
     mapping(address => mapping(uint256 => uint256)) public safePermissions;
+    mapping(address => mapping(uint256 => Policy[])) public safePolicies;
     //prevent replay
-    mapping(address => uint256) public nonce;
-    mapping (uint256 => mapping (uint256 => uint256)) public permissionsTemplates;
+    mapping(address => uint256) public nonces;
+    mapping(uint256 => mapping(uint256 => uint256)) public permissionsTemplates;
     uint256 public permissionTemplatePointer;
-    mapping (uint256 => mapping (uint256 => uint256)) public policyTemplates;
+    mapping(uint256 => Policy[]) public policyTemplates;
     uint256 public policyTemplatePointer;
 
     error previouslyInitialized();
-
-    constructor() payable EIP712("Keisu Capital", "0.1") {}
-
+    error invalidSignature();
+    error invalidRole();
+    error policyNotMet(uint256 role);
+    error insufficientConfirmations();
     //DO NOT override roles 0-1 or funds will be locked forever
     //there is validation for this to prevent attacks in editing functions after this
     function initialize(
@@ -47,47 +57,138 @@ contract OrgValidatorCore is EIP712 {
         Permission[] memory _orgPermissions,
         string memory _orgName
     ) public {
-        if (permissionTemplatePointer != 0) {
+        if (domainSeperator != bytes32(0)) {
             revert previouslyInitialized();
         }
+        domainSeperator = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(_orgName)),
+                keccak256("0.1"),
+                block.chainid,
+                address(this)
+            )
+        );
         orgName = _orgName;
         for (uint256 i = 0; i < _roleMemberships.length; ++i) {
-            roleMemberships[_roleMemberships[i].role][
-                _roleMemberships[i].member
-            ] = true;
+            roleMemberships[_roleMemberships[i].role][_roleMemberships[i].member] = true;
         }
 
         for (uint256 i = 0; i < _orgPermissions.length; ++i) {
-            permissionsTemplates[0][_orgPermissions[i].role] = _orgPermissions[i]
-                .confirmations;
+            permissionsTemplates[0][_orgPermissions[i].role] = _orgPermissions[i].confirmations;
         }
     }
 
-    function validateAuthorizationOrg() internal {
+    function modifyRoleMembership(Membership[] memory _roleMemberships) internal {
+        for (uint256 i = 0; i < _roleMemberships.length; ++i) {
+            roleMemberships[_roleMemberships[i].role][_roleMemberships[i].member] = !roleMemberships[_roleMemberships[i].role][
+                _roleMemberships[i].member
+            ];
+        }
+    }
+
+    function isMember(uint256 _role, address _member) public view returns (bool) {
+        return roleMemberships[_role][_member];
+    }
+
+    //@TODO make this function more efficient (O(n^2) bad)
+    //gets number of confirmations by role for an array of signatures
+    //signatures should be validated before calling this function
+    function getRoleCounts(Signature[] memory signatures) internal pure returns (ConfirmationCount[] memory) {
+        ConfirmationCount[] memory confirmationCounts = new ConfirmationCount[](signatures.length);
+        //loop through signatures and load into confirmationCounts
+        for (uint256 i = 0; i < signatures.length; ++i) {
+            //try to find existing role in confirmationCounts, if not just put it at the end
+            uint256 confirmationCountIndex = confirmationCounts.length;
+            for (uint256 j = 0; j < confirmationCounts.length; ++j) {
+                if (confirmationCounts[j].role == signatures[i].actingRole) {
+                    confirmationCountIndex = j;
+                    break;
+                }
+            }
+            //initialize role if first signature (from that role)
+            if (confirmationCounts[confirmationCountIndex].role == 0) {
+                confirmationCounts[confirmationCountIndex].role = uint128(signatures[i].actingRole);
+            }
+            confirmationCounts[confirmationCountIndex].count++;
+        }
+        return confirmationCounts;
+    }
+
+    //@TODO make this function more efficient (O(n^2) bad)
+    function validatePermissionsOrg(ConfirmationCount[] memory confirmationCounts) internal view {
+        //loop through policy template, then policies and check if they are met
+        for (uint256 i = 0; i < policyTemplates[orgPermissions[1]].length; ++i) {
+            for (uint256 j = 0; j < confirmationCounts.length; ++j) {
+                if (confirmationCounts[j].role == policyTemplates[orgPermissions[1]][i].role) {
+                    if (confirmationCounts[j].count < policyTemplates[orgPermissions[1]][i].requiredConfirmations) {
+                        revert policyNotMet(confirmationCounts[j].role);
+                    }
+                }
+            }
+        }
+        for (uint256 i = 0; i < orgPolicies.length; ++i) {
+            for (uint256 j = 0; j < confirmationCounts.length; ++j) {
+                if (confirmationCounts[j].role == orgPolicies[i].role) {
+                    if (confirmationCounts[j].count < orgPolicies[i].requiredConfirmations) {
+                        revert policyNotMet(confirmationCounts[j].role);
+                    }
+                }
+            }
+        }
+        //loop through confirmationCounts and look for something that has sufficient confirmations
+        //check template first, then additional permissions
+        for (uint256 i = 0; i < confirmationCounts.length; ++i) {
+            if (
+                confirmationCounts[i].count >= permissionsTemplates[orgPermissions[0]][confirmationCounts[i].role] ||
+                confirmationCounts[i].count >= orgPermissions[confirmationCounts[i].role]
+            ) {
+                return;
+            }
+        }
+        revert insufficientConfirmations();
+    }
+
+    //this function returns nothing. the safe contract should revert passing through the error if a call to this function fails
+    function validateAuthorizationMembership(Membership[] memory changes, Signature[] memory signatures)
+        internal
+    {
         unchecked {
-                    address recoveredAddress = ecrecover(
+            //validate and recover addresses from signatures
+            for (uint256 i = 0; i < signatures.length; ++i) {
+                address recoveredAddress = (
+                    ecrecover(
                         keccak256(
                             abi.encodePacked(
                                 "\x19\x01",
-                                _domainSeparatorV4(),
+                                domainSeperator,
                                 keccak256(
                                     abi.encode(
                                         keccak256(
-                                            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+                                            "authorizeMembershipChanges(address targetOrg,Membership[] changes,uint256 actingRole,address signer,uint256 nonce)"
                                         ),
-                                        owner,
-                                        spender,
-                                        value,
-                                        nonces[owner]++,
-                                        deadline
+                                        address(this),
+                                        changes,
+                                        signatures[i].actingRole,
+                                        signatures[i].signer,
+                                        nonces[signatures[i].signer]++
                                     )
                                 )
                             )
                         ),
-                        v,
-                        r,
-                        s
-                    );
-
+                        signatures[i].v,
+                        signatures[i].r,
+                        signatures[i].s
+                    )
+                );
+                if (recoveredAddress != signatures[i].signer) {
+                    revert invalidSignature();
+                }
+                if (!isMember(signatures[i].actingRole, signatures[i].signer)) {
+                    revert invalidRole();
+                }
             }
+            validatePermissionsOrg(getRoleCounts(signatures));
+        }
+    }
 }
