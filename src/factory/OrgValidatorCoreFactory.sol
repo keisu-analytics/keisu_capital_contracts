248f35bc647170c7110fca23cf9239ea96b4025a
DyedM1 <henry@hyf.one>
2022-09-11 18:50:52 -0400

    add OrgValidatorCoreFactory

---
+++ src/factory/OrgValidatorCoreFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+import "../proxy/TransparentStaticProxy.sol";
+import "../core/OrgValidatorCore.sol";
+
+contract OrgValidatorCoreFactory {
+    address public implementation;
+
+    constructor() payable {
+        implementation = address(new OrgValidatorCore());
+    }
+
+    function createOrgValidatorCore(
+        OrgValidatorCore.Membership[] memory _roleMemberships,
+        OrgValidatorCore.Permission[] memory _orgPermissions,
+        string memory _orgName
+    ) public returns (address) {
+        OrgValidatorCore proxy = OrgValidatorCore(address(new TransparentStaticProxy(implementation)));
+        proxy.initialize(_roleMemberships, _orgPermissions, _orgName);
+        return address(proxy);
+    }
+}
