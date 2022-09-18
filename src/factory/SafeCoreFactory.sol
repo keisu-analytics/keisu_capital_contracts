c8e87646a314b8a9f4f6ebba3babd1c2a602c991
DyedM1 <henry@hyf.one>
2022-09-18 14:34:23 -0400

    add SafeCoreFactory

---
+++ src/factory/SafeCoreFactory.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+import "../proxy/TransparentStaticProxy.sol";
+import "../core/SafeCore.sol";
+
+contract SafeCoreFactory {
+    address public implementation;
+
+    constructor() payable {
+        implementation = address(new SafeCore());
+    }
+
+    function createSafeCore(
+        address org
+    ) public returns (address) {
+        SafeCore proxy = SafeCore(address(new TransparentStaticProxy(implementation)));
+        proxy.initialize(org);
+        return address(proxy);
+    }
+}
