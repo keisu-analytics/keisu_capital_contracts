16be35d449706686b1e531ce66ac5e942bfdae26
DyedM1 <henry@hyf.one>
2022-09-18 14:33:08 -0400

    Add factory deploy script

---
+++ script/DeployFactory.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import "src/factory/OrgValidatorCoreFactory.sol";
+import "src/factory/SafeCoreFactory.sol";
+contract DeployScript is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        OrgValidatorCoreFactory validatorfactory = new OrgValidatorCoreFactory();
+        SafeCoreFactory safefactory = new SafeCoreFactory();
+
+        vm.stopBroadcast();
+
+    }
+}
